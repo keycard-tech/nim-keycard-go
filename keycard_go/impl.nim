d9f628f7d59269a29c223efd95542683696959dd
Igor Sirotin <sirotin@status.im>
2025-01-15 23:30:55 +0000

    feat: add api v2 methods

--- keycard_go/impl.nim
+++ keycard_go/impl.nim
@@ -1,6 +1,8 @@
 # go functions do not raise nim exceptions and do not interact with the Nim gc
 {.push raises: [], gcsafe.}
 
+# NOTE: Methods here begin with "Keycard" to avoid name conflicts with status-go
+
 type KeycardSignalCallback* = proc(signal: cstring): void {.cdecl, gcsafe, raises: [].}
 
 proc free*(param: pointer) {.importc: "Free".}
@@ -10,6 +12,8 @@ proc keycardInitFlow*(storageDir: cstring): cstring {.importc: "KeycardInitFlow"
 proc keycardStartFlow*(flowType: cint, jsonParams: cstring): cstring {.importc: "KeycardStartFlow".}
 proc keycardResumeFlow*(jsonParams: cstring): cstring {.importc: "KeycardResumeFlow".}
 proc keycardCancelFlow*(): cstring {.importc: "KeycardCancelFlow".}
+proc keycardInitializeRPC*(): cstring {.importc: "KeycardInitializeRPC".}
+proc keycardCallRPC*(params: cstring): cstring {.importc: "KeycardCallRPC".}
 
 # availale in test mode only
 proc mockedLibRegisterKeycard*(cardIndex: cint, readerState: cint, keycardState: cint, mockedKeycard: cstring, mockedKeycardHelper: cstring): cstring {.importc: "MockedLibRegisterKeycard".}
